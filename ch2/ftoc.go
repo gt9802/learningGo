76732a0a3df662557ae8be0c6dc97d4f07a95b42
Milan George Thomas <milangeorge.thomas@planvisage.com>
2025-07-18 13:15:13 +0530

    tToc

---
+++ ch2/ftoc.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	const freezingF, boilingF = 32.0, 212.0
+	fmt.Printf("%g°F = %g°C\n", freezingF, fToC(freezingF))
+	fmt.Printf("%g°F = %g°C\n", boilingF, fToC(boilingF))
+}
+
+func fToC(f float32) float32 {
+	return (f - 32) * 5 / 9
+}
